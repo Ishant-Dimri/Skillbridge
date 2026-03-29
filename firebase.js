3f7ec2872049c8a4df7d72285b9a28534d50679d
Ishant-Dimri <ishantdimri@gmail.com>
2026-03-29 11:32:28 +0530

    Add Firebase initialization and configuration

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "skillbridge-app.firebaseapp.com",
+  projectId: "skillbridge-app",
+  storageBucket: "skillbridge-app.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
